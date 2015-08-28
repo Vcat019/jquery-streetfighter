a21333153dfaec72f7e6ccf2c75a27a70eb231dc
vcat019 <vcat019@gmail.com>
2015-08-27 22:19:09 -0400

    Initial commit

--- js/app.js
+++ js/app.js
@@ -23,7 +23,21 @@ $('.ryu').mouseenter(function() {
 .mouseup(function() {
 	$('.ryu-throwing').hide();
 	$('.ryu-ready').show();
+  });
+$(document).keydown(function(event) {
+	if(event.which==88) {
+		$('.ryu-still, .ryu-throwing, .ryu-ready').hide();
+		$('.ryu-cool').show();
+
+	}
+})
+.keyup(function(event) {
+	if(event.which==88) {
+		$('.ryu-cool').hide();
+		$('.ryu-still, .ryu-throwing, .ryu-ready').show();
+	}
 });
+
 });
 function playHadouken () {
 	$('#hadouken-sound')[0].volume=0.5;
